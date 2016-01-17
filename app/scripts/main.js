70a27f53a1b938a4c1c597b4705986d93313cfca
Anand Ramadhin <anandramadhin1@gmail.com>
2016-01-16 21:46:48 -0400

    Added Jquery to hide/show text from buttons. Removed mobile, web and game development pages. Removed links from buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+function init() {
+   $("#mobile").hide(1);
+   $("#web").hide(1);
+   $("#game").hide(1);
+};
+
+$("#web_button").click(function () {
+   $("#web").show('fast');
+   $("#mobile").hide('fast');
+   $("#game").hide('fast');
+});
+
+$("#mobile_button").click(function () {
+   $("#mobile").show('fast');
+   $("#web").hide('fast');
+   $("#game").hide('fast');
+});
+
+$("#game_button").click(function () {
+   $("#game").show('fast');
+   $("#web").hide('fast');
+   $("#mobile").hide('fast');
+});
+
